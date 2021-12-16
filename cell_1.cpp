cb20384651101c163afa97cbb56b8d19f182fdb3
AnalitikGnid <gladin.no@phystech.edu>
2021-12-16 20:51:09 +0300

    pre-final version

---
+++ cell_1.cpp
@@ -0,0 +1,92 @@
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include<iostream>
+
+class Cell {
+    public:
+        bool is_alive;
+        int alive_nb;
+        Cell(): is_alive(false), alive_nb(0){}
+
+    void Print(int i, int j, bool t, int len, sf::RenderWindow &window){
+        sf::RectangleShape rectangle(sf::Vector2f(len, len));
+        rectangle.move(i * len, j * len);
+
+        if(t) rectangle.setFillColor(sf::Color::Black);
+        else  rectangle.setFillColor(sf::Color::White);
+
+        window.draw(rectangle);
+        window.display();
+        }
+
+
+};
+
+struct Field {
+
+        int length;
+        int n, m;
+
+        std::vector <std::vector <Cell> > grind;
+
+        Field(int n_, int m_, int length_): n(n_), m(m_), length(length_){
+                grind.resize(n);
+                for(int i = 0; i < n; i++) {
+                        grind[i].resize(m);
+                }
+        }
+
+        void Set_Position(std::vector< std::pair<int, int> > base, int sdvigx, int sdvigy){
+		for (int k = 0; k < base.size(); k++){
+			grind[base[k].first + sdvigx][base[k].second + sdvigx].is_alive = true;
+		}
+	}
+
+
+
+
+        void Print(sf::RenderWindow &window) const{
+        for (int i = 0; i < n; i++){
+                        for (int j = 0; j < m; j++){
+                if(grind[i][j].is_alive){
+                    sf::RectangleShape rectangle(sf::Vector2f(length, length));
+                    rectangle.move(i * length, j * length);
+
+                    rectangle.setFillColor(sf::Color::Black);
+
+                    window.draw(rectangle);
+                }
+                        }
+                }
+                window.display();
+        }
+	void Recount(){
+                for (int i = 1; i < n - 1; i++){
+            		for (int j = 1; j < m - 1; j++){
+                		grind[i][j].alive_nb  = grind[i-1][j-1].is_alive + grind[i-1][j].is_alive + grind[i-1][j+1].is_alive
+                                      		      + grind[i][j-1].is_alive + grind[i][j+1].is_alive
+                                     		      + grind[i+1][j-1].is_alive + grind[i+1][j].is_alive + grind[i+1][j+1].is_alive;
+                //if(grind[i][j].alive_nb >= 2) std::cout << "YEs: " << i << " " << j << std::endl;
+            }
+        }
+                for (int i = 1; i < n - 1; i++){
+            for (int j = 1; j < m - 1; j++){
+                if (grind[i][j].is_alive){
+                    if (grind[i][j].alive_nb < 2 || grind[i][j].alive_nb > 3)
+                        grind[i][j].is_alive = false;
+                }
+                else{
+                    if (grind[i][j].alive_nb == 3)
+                        grind[i][j].is_alive = true;
+
+                }
+            }
+        }
+
+        }
+};
+
+
+                                                                                                                                          //     91,0-1        Bot
+
+
---
+++ main.cpp
@@ -0,0 +1,120 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <string>
+
+
+#include </home/nikita/inf_proj/cell_1.cpp>
+
+
+std::vector< std::pair<int, int> > Read(std::string filename){
+        std::vector< std::pair<int, int> > base;
+	std::string line;
+
+       	std::ifstream in(filename); // окрываем файл для чтения
+        int i_line = 0;
+        if (in.is_open()){
+                while (getline(in, line)){
+                        for(int j_col = 0; j_col < line.size(); j_col++){
+                                if(line[j_col] == 'O') { //Большая О(как оборона), не ноль
+                                        base.push_back({i_line, j_col});
+                                }
+                        }
+                        i_line++;
+                }
+
+        }
+        in.close(); // закрываем файл
+	return base;
+}
+
+int main()
+{
+	int window_length = 1500;
+	int window_height = 1500;
+	int cell_size = 10;
+	sf::RenderWindow window(sf::VideoMode(window_length, window_height), "SFML works!");
+	Field f(window_length / cell_size, window_height / cell_size, cell_size);
+
+
+	std::vector< std::pair<int, int> > base1 = Read("database/in/1.txt");
+	//std::vector< std::pair<int, int> > base2 = Read("database/in/2.txt");
+
+	f.Set_Position(base1, 25, 25);
+	//f.Set_Position(base2, 75, 75);
+
+
+	bool Ispause = 1;
+    	bool Isspace = 0;
+	while (window.isOpen())
+	{
+		sf::Event event;
+        	while (window.pollEvent(event))
+        	{
+            		if (event.type == sf::Event::Closed)
+               			window.close();
+        	}
+
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
+                sf::sleep(sf::milliseconds(10));
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)){
+                f.Print(window);
+
+                sf::sleep(sf::milliseconds(100));
+            }
+
+
+            else if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+                sf::Vector2i localPos = sf::Mouse::getPosition(window);
+                sf::sleep(sf::milliseconds(00));
+                int ii = localPos.x / cell_size;
+                int jj = localPos.y / cell_size;
+		if (!(ii <= 0 || jj <= 0 || ii >= (window_height / cell_size - 1) || jj >= (window_length / cell_size - 1))) 
+                if(f.grind[ii][jj].is_alive == 0){
+                    f.grind[ii][jj].is_alive = true;
+                    //f.grind[ii][jj].Print(ii, jj, 1, cell_size, window);
+                    f.Print(window);
+
+                }
+            }
+            else if(sf::Mouse::isButtonPressed(sf::Mouse::Right)){
+                sf::Vector2i localPos = sf::Mouse::getPosition(window);
+                sf::sleep(sf::milliseconds(100));
+                int ii = localPos.x / cell_size;
+                int jj = localPos.y / cell_size;
+
+                if(f.grind[ii][jj].is_alive){
+                        sf::sleep(sf::milliseconds(10));
+                    f.grind[ii][jj].is_alive = false;
+                    f.grind[ii][jj].Print(ii, jj, 0, cell_size, window);
+                    //f.Print(window);
+
+                }
+            }
+
+
+        }
+        else{
+            window.clear(sf::Color::White);
+            sf::sleep(sf::milliseconds(100));
+
+            //f.Print(window);
+            f.Recount();
+            f.Print(window);
+
+        }
+
+        //sf::sleep(sf::milliseconds(100));
+
+
+
+    }
+
+    return 0;
+}
